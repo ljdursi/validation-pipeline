a0953f9f5d78e8c413fcd02e26889065706031ae
Jonathan Dursi <jonathan@dursi.ca>
2015-11-10 16:36:57 -0500

    Improve merging script, give a better name

--- scripts/merge-validation-selection.sh
+++ scripts/merge-validation-selection.sh
@@ -16,6 +16,6 @@ VALIDATION=$1
 SELETION=$2
 
 join -j 1\
-    <( grep -v "^#" ${VALIDATION} | awk '$8 !~ /^DP=[0-9];/ && $10 !~ /^\.\/\./ && $11 !~ /^\.\/\./ {id=$1"_"$2; if ($5 == "." || $11 ~ /0\/0:/) { filter="NOTSEEN";} else if ($10 !~ /0\/0:/) { filter = "GERMLINE" } else filter="PASS"; printf "%s\t%s\t%s\n", id, filter, $0}' | sort ) \
-    <( grep -v "^#" ${SELETION} | sed -e 's/^chr//' | awk '{id=$1"_"$2; printf "%s\t%s\n", id, $0}' | sort ) \
+    <( grep -v "^#" ${VALIDATION} | awk '$8 !~ /^DP=[0-9];/ && $10 !~ /^\.\/\./ && $11 !~ /^\.\/\./ {id=$1"_"$2; if ($5 == "." || $11 ~ /0\/0:/) { filter="NOTSEEN";} else if ($10 !~ /0\/0:/) { filter = "GERMLINE" } else filter="PASS"; printf "%s\t%s\t%s\n", id, filter, $0}' | sort -k 1,1 ) \
+    <( grep -v "^#" ${SELETION} | sed -e 's/^chr//' | awk '{id=$1"_"$2; printf "%s\t%s\n", id, $0}' | sort -k 1,1 ) \
     | awk '{printf "%s\t%d\t%s\t%s\t%s\t%s\t%s\t%s;%s\t%s\t%s\t%s\n", $3, $4, $5, $6, $18, $8, $2, $10, $21, $11, $12, $13}'
