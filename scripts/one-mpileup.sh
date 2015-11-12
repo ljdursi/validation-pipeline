c3f712c5c4e7f9c635f2c0aaa659472fd5570e67
Jonathan Dursi <jonathan@dursi.ca>
2015-11-12 17:43:16 -0500

    fix typos

--- scripts/one-mpileup.sh
+++ scripts/one-mpileup.sh
@@ -8,7 +8,7 @@ module load bcftools/1.1
 
 DEFREF=/oicr/data/pancanxfer/validation/reference/bwa-0.6.2/genome.fa
 
-if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] \
+if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] \
     || [ ! -f "$1" ] || [ ! -f "$2" ] || [ ! -f "$3" ] || [ ! -f "$4" ]
 then
     echo "$0 - performs variant calling on two BAMS"
@@ -20,14 +20,14 @@ NORMAL=$1
 TUMOUR=$2
 REGIONS=$3
 TARGETS=$4
-REFERENCE=${5-$DEFREF}
-
-mpileupdir=/oicr/data/pancanxfer/validation/mpileups
+MPILEUPFILE=$5
+REFERENCE=${6-$DEFREF}
 
 mkdir -p ${mpileupdir}
 mpileupfile=${mpileupdir}/$( basename $OUTPUT .vcf ).mpileup
 
 if [ ! -f ${mpileupfile} ]
 then
-    samtools mpileup --output-tags DP,DP4,SP -C50 --positions $REGIONS -gf $REFERENCE $NORMAL $TUMOUR > ${mpileupfile}
+    samtools mpileup --max-depth 8000 --output-tags DP,DP4,SP -C50 \
+        --positions $REGIONS -g --fasta-ref $REFERENCE $NORMAL $TUMOUR > ${MPILEUPFILE}
 fi
