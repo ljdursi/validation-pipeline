09c468815bf2163217d847935b19455a59f379bd
Jonathan Dursi <jonathan@dursi.ca>
2015-11-17 15:33:46 -0500

    Modify call to sga annotate to lift max alignments limit; use all selected calls

--- scripts/one-sga-annotate.sh
+++ scripts/one-sga-annotate.sh
@@ -5,7 +5,7 @@ module use /.mounts/labs/simpsonlab/modules/
 module load parallel
 module load gcc/4.8.1
 
-DEFREF=/oicr/data/pancanxfer/validation/reference/bwa-0.6.2/genome.fa.gz
+DEFREF=/oicr/data/pancanxfer/validation/reference/bwa-0.6.2/genome.fa
 SGABIN=/.mounts/labs/simpsonlab/users/jsimpson/code/sga/src/build/SGA/sga 
 
 if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] \
@@ -25,6 +25,6 @@ REFERENCE=${5-$DEFREF}
 
 if [ ! -f "$OUTPUT_VCF" ]
 then
-    ${SGABIN} somatic-variant-filters --annotate-only -t 4 \
-        --tumor $TUMOUR_BAM --normal $NORMAL_BAM --reference $REFERENCE $INPUT_VCF > $OUTPUT_VCF
+    ${SGABIN} somatic-variant-filters --annotate-only --threads=4 --max-alignments 50000 \
+        --tumor-bam=$TUMOUR_BAM --normal-bam=$NORMAL_BAM --reference=$REFERENCE <( sed -e 's/^chr//' $INPUT_VCF ) > $OUTPUT_VCF
 fi
