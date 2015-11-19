052c393b7b9a935cf61b1807f42363874c962d24
Jonathan Dursi <jonathan@dursi.ca>
2015-11-19 18:24:46 -0500

    Start turning the re-mapping pipeline into something that launches parallel jobs

--- scripts/one_bam_to_fastq.sh
+++ scripts/one_bam_to_fastq.sh
@@ -1,55 +1,37 @@
-#!/bin/bash
+#!/bin/bash -l 
 module purge 
 module unuse /oicr/local/boutroslab/Modules/modulefiles
 module use /.mounts/labs/simpsonlab/modules/
+module load samtools/1.2
+module load java/1.8.0_40
+module load picard/1.92
 module load parallel
 
-function picardbamtofastq {
-    FASTQDIR=$1
-    BAMFILE=$2
-    MEM=${3-4g}
+DEFNJOBS=8
 
-    BASE=${FASTQDIR}/$( basename $BAMFILE .bam )
-
-    if [ ! -d ${BASE} ]
-    then
-        module load samtools/1.2
-        module load java/1.8.0_40
-        module load picard/1.92
-        mkdir ${BASE}
-        samtools view -H $BAMFILE > ${BASE}/bam-header.txt
-        echo "java -Xmx${MEM} -Xms${MEM} -jar ${PICARDROOT}/SamToFastq.jar VALIDATION_STRINGENCY=LENIENT \
-                OUTPUT_PER_RG=true OUTPUT_DIR=${BASE} INPUT=${BAMFILE}"
-        java -Xmx${MEM} -Xms${MEM} -jar ${PICARDROOT}/SamToFastq.jar VALIDATION_STRINGENCY=LENIENT \
-                OUTPUT_PER_RG=true OUTPUT_DIR=${BASE} INPUT=${BAMFILE} &&
-            for file in ${BASE}/*.fastq; do gzip $file; done
-    fi  
-}
-
-export -f picardbamtofastq
-
-if [ $# -eq 0 ]
+if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ ! -f $1 ] 
 then
     echo "$0 - converts BWA ALN aligned BAMS to FASTQ"
-    echo "Usage: $0 bamindir fastqoutdir [njobs=8]"
+    echo "     BAMS are separated by RG so that they can be recombined with RG information "
+    echo "Usage: $0 bamindir fastqoutdir [njobs=${DEFNJOBS}]"
+    echo "invocation was: $0 $1 $2 $3"
     exit 
 fi
 
-if [ -z "$1" ]
-then
-    echo "No BAM input dir supplied"
-    exit
-fi
-
-if [ -z "$2" ]
-then
-    echo "No FASTQ output dir supplied"
-    exit
-fi
-
-BAMDIR=$1
+BAMFILE=$1
 FASTQDIR=$2
+NJOBS=${3-8}
+MEM=4g
 
-JOBS=${3-8}
-
-find ${BAMDIR} -name *.bam -print | parallel -j $JOBS picardbamtofastq ${FASTQDIR} {} 4g
+BASE=${FASTQDIR}/$( basename $BAMFILE .bam )
+if [ ! -d ${BASE} ]
+then
+    echo "Converting ${BAMFILE} to fastq into ${FASTQDIR}/${BASE}"
+    mkdir ${BASE}
+    samtools view -H $BAMFILE > ${BASE}/bam-header.txt
+    echo "java -Xmx${MEM} -Xms${MEM} -jar ${PICARDROOT}/SamToFastq.jar VALIDATION_STRINGENCY=LENIENT \
+            OUTPUT_PER_RG=true OUTPUT_DIR=${BASE} INPUT=${BAMFILE}"
+    java -Xmx${MEM} -Xms${MEM} -jar ${PICARDROOT}/SamToFastq.jar VALIDATION_STRINGENCY=LENIENT \
+            OUTPUT_PER_RG=true OUTPUT_DIR=${BASE} INPUT=${BAMFILE} &&
+    for file in ${BASE}/*.fastq; do gzip $file; done
+fi  
