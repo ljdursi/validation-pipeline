7ef738a33df70e4cecb1a6ca4092fc62042f3856
Jonathan Dursi <jonathan@dursi.ca>
2016-07-23 22:40:15 -0400

    Add headers to output

--- scripts/one-indel-call.sh
+++ scripts/one-indel-call.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -l
 module purge 
-module unuse /oicr/local/boutroslab/Modules/modulefiles
-
-module load python
-module load gcc/4.8.1
 module use /.mounts/labs/simpsonlab/modules/
-module load openblas
 module load python-packages/2
 
 if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ ! -f "$1" ] 
@@ -21,7 +16,27 @@ OUTPUT_VCF=$2
 
 if [ ! -f "$OUTPUT_VCF" ]
 then
+    cat > ${OUTPUT_VCF} <<EOF
+##fileformat=VCFv4.1
+##INFO=<ID=Callers,Number=.,Type=String,Description="Callers that made this call">
+##INFO=<ID=NormalEvidenceReads,Number=1,Type=Integer,Description="Number of reads in normal sample supporting the alt allele">
+##INFO=<ID=TumourEvidenceReads,Number=1,Type=Integer,Description="Number of reads in tumour sample supporting the alt allele">
+##INFO=<ID=TumourReads,Number=1,Type=Integer,Description="Total number of reads in tumour sample">
+##INFO=<ID=NormalReads,Number=1,Type=Integer,Description="Total number of reads in normal sample">
+##INFO=<ID=Validation_status,Number=.,Type=String,Description="Validation status, as info field">
+##INFO=<ID=RepeatRefCount,Number=1,Type=Integer,Description="Reference repeat unit count at this location">
+##INFO=<ID=RepeatUnit,Number=1,Type=String,Description="Reference repeat unit">
+##INFO=<ID=3pContext,Number=1,Type=String,Description="Genomic context at 3-end">
+##INFO=<ID=5pContext,Number=1,Type=String,Description="Genomic context at 3-end">
+##FILTER=<ID=LOWSUPPORT,Description="Insufficient depth to validate the call">
+##FILTER=<ID=NOTSEEN,Description="Variant not seen in Tumour">
+##FILTER=<ID=STRANDBIAS,Description="Too much strand bias in Tumour sample to validate the call">
+##FILTER=<ID=NORMALEVIDENCE,Description="Evidence for presence in normal sample">
+##FILTER=<ID=GERMLINE,Description="Germline het or hom">
+EOF
     ./scripts/snv_indel_call.py --error 0.01 --callthreshold 0.02 --mindepth 25 --strandbias -1 --germlineprob 0.05 \
         -i ${INPUT_VCF} \
-        | sort -k1,1 -k2,2n | sed -e 's/^\([1-9XYh]\)/chr\1/' > ${OUTPUT_VCF}
+        | sed -e 's/;;/;/' \
+        | grep -v "^##" \
+        | sort -k1,1 -k2,2n | sed -e 's/^\([1-9XYh]\)/chr\1/' >> ${OUTPUT_VCF}
 fi
--- scripts/one-snv-call.sh
+++ scripts/one-snv-call.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -l
 module purge 
-module unuse /oicr/local/boutroslab/Modules/modulefiles
-
-module load python
-module load gcc/4.8.1
 module use /.mounts/labs/simpsonlab/modules/
-module load openblas
 module load python-packages/2
 
 if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ ! -f "$1" ] || [ ! -f "$2" ] || [ ! -f "$3" ]
@@ -23,7 +18,24 @@ OUTPUT_VCF=$4
 
 if [ ! -f "$OUTPUT_VCF" ]
 then
+    cat > ${OUTPUT_VCF} <<EOF
+##fileformat=VCFv4.1
+##INFO=<ID=Callers,Number=.,Type=String,Description="Callers that made this call">
+##INFO=<ID=NormalEvidenceReads,Number=2,Type=Integer,Description="Number of reads in normal sample supporting the alt allele (forward,backward)">
+##INFO=<ID=TumourEvidenceReads,Number=2,Type=Integer,Description="Number of reads in tumour sample supporting the alt allele (forward,backward)">
+##INFO=<ID=TumourReads,Number=1,Type=Integer,Description="Total number of reads in tumour sample">
+##INFO=<ID=NormalReads,Number=1,Type=Integer,Description="Total number of reads in normal sample">
+##INFO=<ID=Validation_status,Number=.,Type=String,Description="Validation status, as info field">
+##FILTER=<ID=LOWDEPTH,Description="Insufficient depth to validate the call">
+##FILTER=<ID=NOTSEEN,Description="Variant not seen in Tumour">
+##FILTER=<ID=STRANDBIAS,Description="Too much strand bias in Tumour sample to validate the call">
+##FILTER=<ID=NORMALEVIDENCE,Description="Evidence for presence in normal sample">
+##FILTER=<ID=GERMLINE,Description="Germline het or hom">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
+EOF
     ./scripts/snv_readcounts.py ${INPUT_VCF} ${NORMAL_RC} ${TUMOUR_RC} | \
-        ./scripts/snv_indel_call.py --error 0.01 --callthreshold 0.02 --strandbias -1 --germlineprob 0.01 --mindepth 25 \
-        | sort -k1,1 -k2,2n  > ${OUTPUT_VCF}
+        ./scripts/snv_indel_call.py --error 0.01 --callthreshold 0.02 --strandbias 0.001 --germlineprob 0.01 --mindepth 25 \
+        | sed -e 's/;;/;/' \
+        | grep -v "^#" \
+        | sort -k1,1 -k2,2n  >> ${OUTPUT_VCF}
 fi
