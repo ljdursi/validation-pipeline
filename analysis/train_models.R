923e916c0426efa7008fde316f5d6a1cb09cbb25
Jonathan Dursi <jonathan@dursi.ca>
2016-07-27 11:09:14 -0400

    For final release

--- analysis/train_models.R
+++ analysis/train_models.R
@@ -1,9 +1,6 @@
 source('analysis/precision_recall.R')
 source('analysis/estimate_accuracies.R')
 
-#allfour <- as.formula("validate_true ~ (dkfz + sanger + smufin)*(wgs_tvaf*wgs_tvardepth + wgs_nvaf*wgs_nvardepth + varlen + cosmic:dbsnp + thousand_genomes + repeat_masker + repeat_count + broad_snowman)")
-#nosmufin<- as.formula("validate_true ~ (dkfz + sanger)*(wgs_tvaf*wgs_tvardepth + wgs_nvaf*wgs_nvardepth + varlen + cosmic:dbsnp + thousand_genomes + repeat_masker + repeat_count + broad_snowman)")
-#nobroad <- as.formula("validate_true ~ (dkfz + sanger + smufin)*(wgs_tvaf*wgs_tvardepth + wgs_nvaf*wgs_nvardepth + varlen + cosmic:dbsnp + thousand_genomes + repeat_masker + repeat_count)")
 allfour <- as.formula("validate_true ~ (dkfz + sanger + smufin)*(wgs_tvaf + wgs_tvardepth + wgs_nvaf + wgs_nvardepth + varlen + cosmic + dbsnp + thousand_genomes + repeat_masker + repeat_count + broad_snowman)")
 nosmufin<- as.formula("validate_true ~ (dkfz + sanger)*(wgs_tvaf + wgs_tvardepth + wgs_nvaf + wgs_nvardepth + varlen + cosmic + dbsnp + thousand_genomes + repeat_masker + repeat_count + broad_snowman)")
 nobroad <- as.formula("validate_true ~ (dkfz + sanger + smufin)*(wgs_tvaf + wgs_tvardepth + wgs_nvaf  + wgs_nvardepth + varlen + cosmic + dbsnp + thousand_genomes + repeat_masker + repeat_count)")
@@ -13,7 +10,7 @@ train_stacked_model <- function(model.formula, indels, indel_calls) {
   model <- glmnetlearn(model.formula, copy_indels)
 }
 
-train_stacked_models <- function(indels, indel_calls, dir='../consensus/initial_consensus/models/') {
+train_stacked_models <- function(indels, indel_calls, dir='../consensus/final_consensus/models/') {
   formula <- allfour
   model <- train_stacked_model(formula, indels, indel_calls)
   predict_function <- glmnetpredict
